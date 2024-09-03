881424b249d017cb6fd2b7243480ce17db915f64
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2024-09-03 21:57:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect  =()=>{
 const port = process.env.PORT || 8800;
 
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/users",userRoutes);
