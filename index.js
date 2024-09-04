c5ccd4cec8fbc079e7e68f2dae4af051bba10d00
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2024-09-04 21:08:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect  =()=>{
 const port = process.env.PORT || 8800;
 
 
-app.use(cors())
+app.use(cors({ origin: https://frontend-5duj.onrender.com , methods: "GET,POST,PUT,DELETE", credentials: true }))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/users",userRoutes);
