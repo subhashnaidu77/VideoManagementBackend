821e9c3d316da0062295e9b59fe051b68d5c45dd
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2024-10-07 21:44:54 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,6 +61,7 @@ export const signin= async(req,res,next)=>{
               .cookie("access_token", token, {
                 httpOnly: true,
               })
+            
               .status(200)
               .json(savedUser._doc);
           }
