fc0b9770237401b7ce459df96c8f232bbd56613b
Subhash <subhashnaidu77>
2024-10-25 19:53:08 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,10 +30,6 @@ export const signin= async(req,res,next)=>{
           res
       .cookie("access_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-
-          sameSite:'None',
-           domain: '.onrender.com' 
       })
       .status(200)
       .json(others);
